154750136114788cca491380690c507be904ca81
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-09-08 19:13:47 +0530

    add files

---
+++ block-BRaabk/code/our-menu/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Tags from './Tags'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Tags />
+        </div>
+    )
+}
+
+export default App;
